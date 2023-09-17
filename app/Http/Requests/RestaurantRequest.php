eb35a91f24ee5b76909f3eabe2724802dde24d5d
Amani Nyumu <amaninyumu1@gmail.com>
2023-09-17 12:29:41 +0200

    add restaurant request

---
+++ app/Http/Requests/RestaurantRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+class RestaurantRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'restaurant_name'=>['required','string','min:4',Rule::unique('restaurants')->ignore($this->route()->parameter('restaurant'))],
+            'whatapp'=>['required','string',Rule::unique('restaurants')->ignore($this->route()->parameter('restaurant'))],
+            'adresse'=>['required','string',Rule::unique('restaurants')->ignore($this->route()->parameter('restaurant'))],
+            'restaurant_tel'=>['required',Rule::unique('restaurants')->ignore($this->route()->parameter('restaurant'))],
+            'restaurant_email'=>['required','email',Rule::unique('restaurants')->ignore($this->route()->parameter('restaurant'))],
+            'longitude'=>['numeric',Rule::unique('restaurants')->ignore($this->route()->parameter('restaurant'))],
+            'latitude'=>['numeric',Rule::unique('restaurants')->ignore($this->route()->parameter('restaurant'))],
+            'logo'=>['required','image', 'max:2000', 'mimes:jpge,jpg,png']
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'error' => true,
+            'message' => "Erreur de Creation du Restaurant",
+            'errorList' => $validator->errors()
+        ], 400));
+    }
+}
